19a56a3f1d20a286be2347113e2126286c4f2256
liboyang <liboyang@maxrocky.com>
2018-03-17 14:24:10 +0800

    cs

--- src/main/java/com/beyondli/WorkSpringcloudEurekaApplication.java
+++ src/main/java/com/beyondli/WorkSpringcloudEurekaApplication.java
@@ -3,7 +3,7 @@ package com.beyondli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//1
 @EnableEurekaServer
 @SpringBootApplication
 public class WorkSpringcloudEurekaApplication {
